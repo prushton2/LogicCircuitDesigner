69fb9a1a31e2e4f6cb121d8cb29e565cd1f65c92
prushton2 <peterrushton418@gmail.com>
2023-02-23 13:42:30 -0500

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Workspace from "./components/Workspace"
 
 function App() {
-  return (
-    <div className="App">
-		<Workspace />
-    </div>
-  );
+
+	useEffect(() => {
+		document.title="Logic Circuit Designer"
+	}, [])
+
+	return (
+		<div className="App">
+			<Workspace />
+		</div>
+	);
 }
 
 export default App;
