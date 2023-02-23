7a4b65dc775356fbdbe7ef02e4816bf8d6522aed
prushton2 <peterrushton418@gmail.com>
2023-02-23 14:41:44 -0500

    added styling to switches for hidden mode

--- src/components/IO.tsx
+++ src/components/IO.tsx
@@ -1,14 +1,19 @@
+import "../App.css"
+
 import { useState,useEffect, useContext } from "react";
 import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 import Draggable from "react-draggable";
 
-import { pos } from "../models/pos";
-import { WireContext } from "./Context";
+import { ConfigContext, WireContext } from "./Context";
 
 export const Switch = ({id, onClick}: {id: any, onClick: (id: string) => void}) => {
 
 	const [value, setValue] = useState(false);
 	const {wires, setWires} = useContext(WireContext);
+	const {config, setConfig} = useContext(ConfigContext);
+	
+	const [display, setDisplay] = useState("inline"); //for hiding the gate configuration
+	const [name, setName] = useState(id);
 
 	const updateXarrow = useXarrow();
 
@@ -18,15 +23,21 @@ export const Switch = ({id, onClick}: {id: any, onClick: (id: string) => void}) 
 		setWires(newWires);
 	}, [value])
 
+	useEffect(() => {
+		setDisplay(config["displayMode" as keyof object] === "full" ? "inline": "none");
+	}, [config])
 
 	return (
 
 		<Draggable grid={[5, 5]} onDrag={updateXarrow} onStop={updateXarrow}>
 			<div id={`${id}.Y`} style={{position: "absolute", width: "100px", height: "100px", border: "0px solid red"}}>
-				<button onClick={() => {setValue(!value)}}>SW ({id})</button> <br />{value ? "1":"0"}
+				
+				<input style={{ height: "20px", width: "30px", display: display}} onChange={(e) => {setName(e.target.value)}}></input><br />
+				
+				<button className="invisButton" style={{width: "50px", height: "50px", position: 'absolute', top: "25px", left: "25px"}} onClick={() => {setValue(!value)}}>{name}</button> <br />
 
-				<div id={`${id}.A`} style={{right: "0%", top: "50%", position: "absolute", transform: "translate(0%, -50%)"}}>
-					<button onClick={(e) => onClick(`${id}.Y`)} style={{marginRight: "1.3em"}}>Y</button> <br /> 
+				<div id={`${id}.A`} style={{right: "0px", top: "50%", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<button onClick={(e) => onClick(`${id}.Y`)} style={{marginRight: "1.3em", display: display}}>Y</button> <br /> 
 				</div>
 			</div>
 		</Draggable>
