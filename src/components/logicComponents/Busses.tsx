1e6d3c80448901985a8b2d25ea29ab27f1cde2eb
prushton2 <peterrushton418@gmail.com>
2023-03-02 09:26:10 -0500

    made busses smaller

--- src/components/logicComponents/Busses.tsx
+++ src/components/logicComponents/Busses.tsx
@@ -36,33 +36,33 @@ export function BUS({id, pos, I, onClick, setPos}: {id: string, pos: pos, I: inp
 	return (
 		<div>
 			<Component defaultPos={pos} newPos={(pos) => setPos(pos, id)} setDisplay={(v, d) => setDisplay(d)}>
-				<div style={{width: "90px", height: "400px", border: "5px solid white"}}>
-					<div id={`${id}.A`} style={{left: "0%", top: "25px", position: "absolute", transform: "translate(0%, -50%)"}}>
+				<div style={{width: "90px", height: "205px", border: "5px solid white"}}>
+					<div id={`${id}.A`} style={{left: "0%", top: "15px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}A<button onClick={(e) => onClick(`${id}.A`)} style={{marginLeft: ".3em", display: display}}>A</button><br /> 
 					</div>
-					<div id={`${id}.B`} style={{left: "0%", top: "75px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.B`} style={{left: "0%", top: "40px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}B<button onClick={(e) => onClick(`${id}.B`)} style={{marginLeft: ".3em", display: display}}>B</button><br /> 
 					</div>
-					<div id={`${id}.C`} style={{left: "0%", top: "125px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.C`} style={{left: "0%", top: "65px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}C<button onClick={(e) => onClick(`${id}.C`)} style={{marginLeft: ".3em", display: display}}>C</button><br /> 
 					</div>
-					<div id={`${id}.D`} style={{left: "0%", top: "175px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.D`} style={{left: "0%", top: "90px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}D<button onClick={(e) => onClick(`${id}.D`)} style={{marginLeft: ".3em", display: display}}>D</button><br /> 
 					</div>
-					<div id={`${id}.E`} style={{left: "0%", top: "225px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.E`} style={{left: "0%", top: "115px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}E<button onClick={(e) => onClick(`${id}.E`)} style={{marginLeft: ".3em", display: display}}>E</button><br /> 
 					</div>
-					<div id={`${id}.F`} style={{left: "0%", top: "275px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.F`} style={{left: "0%", top: "140px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}F<button onClick={(e) => onClick(`${id}.F`)} style={{marginLeft: ".3em", display: display}}>F</button><br /> 
 					</div>
-					<div id={`${id}.G`} style={{left: "0%", top: "325px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.G`} style={{left: "0%", top: "165px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}G<button onClick={(e) => onClick(`${id}.G`)} style={{marginLeft: ".3em", display: display}}>G</button><br /> 
 					</div>
-					<div id={`${id}.H`} style={{left: "0%", top: "375px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.H`} style={{left: "0%", top: "190px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						{'\u00A0'}H<button onClick={(e) => onClick(`${id}.H`)} style={{marginLeft: ".3em", display: display}}>H</button><br /> 
 					</div>
 
-					<div id={`${id}.Y`} style={{right: "0%", top: "200px", position: "absolute", transform: "translate(0%, -50%)"}}>
+					<div id={`${id}.Y`} style={{right: "0%", top: "95px", position: "absolute", transform: "translate(0%, -50%)"}}>
 						<button onClick={(e) => onClick(`${id}.Y`)} style={{marginRight: ".3em", display: display}}>Y</button>Y{'\u00A0'}
 					</div>
 
